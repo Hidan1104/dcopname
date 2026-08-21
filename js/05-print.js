0fd37d9d1d916f7598b397f5ddf46d9d91419029
Hidan1104 <aminnursahidan24@gmail.com>
2026-08-21 16:05:24 +0700

    fixit

--- js/05-print.js
+++ js/05-print.js
@@ -95,7 +95,8 @@ function drawCountingSheetCopy(doc, startY, header, rows, dcNama, session, secto
     startY: startY + 9,
     margin: { left: 6, right: 6 },
     styles: { fontSize: 11, cellPadding: 2.2, valign: 'middle' },
-    headStyles: { fillColor: [255,255,255], fontSize: 11.5 },
+    theme: 'grid',
+    headStyles: { fillColor: [255,255,255], textColor: [0,0,0], fontStyle: 'bold', fontSize: 11.5, lineColor: [0,0,0], lineWidth: 0.2 },
     columnStyles: {
       0: { cellWidth: 26 },
       1: { cellWidth: 22, halign: 'center' },
